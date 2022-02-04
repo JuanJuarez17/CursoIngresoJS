d683f56c39e5946f081a99881ca64604ce910870
Juan Juarez <jjuarez@ferrum.com>
2022-02-04 12:32:29 -0300

    Prueba Laburo

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -29,6 +29,8 @@ function mostrarAumento()
 
 	//Revisar que aumento y descuento no se usa *0,1 por ejemplo, sino sumas y restas como se hizo en este.
 
+	//Modificacion PC Laburo
+	
 	//document.getElementById("txtIdResultado").value = "Su sueldo es " + sueldo * (1 + (aumento/100));
 	//Con esto se puede escribir en el cuadro de resultado
 }
