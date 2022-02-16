2446e3ef157a2fb784fad5d52609138bf49c6a5c
Juan Juarez <jjuarez@ferrum.com>
2022-02-16 15:52:47 -0300

    16022022

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,68 @@
-function mostrar()
-{
+function mostrar() {
+    var destino;
+    var cantidadDias;
+    var metodoPago;
+    var costoBruto;
+    var costoNeto;
+    var descuentoDestino;
+    var descuentoPago;
+
+    destino = document.getElementById("Marca").value;
+
+    cantidadDias = prompt("Ingrese la cantidad de dias:");
+    cantidadDias = parseInt(cantidadDias);
+
+    metodoPago = prompt("Ingrese el método de pago:");
+
+    costoBruto = cantidadDias * 100;
+
+    switch (destino) {
+        case "América":
+            descuentoDestino = -0.5;
+            switch (metodoPago) {
+                case "Debito":
+                    descuentoPago = -0.1;
+                    break;
+                default:
+                    descuentoPago = 0;
+                    break;
+            }
+            break;
+        case "África":
+            descuentoDestino = -0.6;
+            switch (metodoPago) {
+                case "Efectivo":
+                case "Mercado pago":
+                    descuentoPago = -0.15;
+                    break;
+                default:
+                    descuentoPago = 0;
+                    break;
+            }
+            break;
+        case "Europa":
+            descuentoDestino = -0.2;
+            switch (metodoPago) {
+                case "Debito":
+                    descuentoPago = -0.15;
+                    break;
+                case "Mercado pago":
+                    descuentoPago = -0.1;
+                    break;
+                default:
+                    descuentoPago = -0.05;
+                    break;
+            }
+            break;
+        default:
+            descuentoDestino = 0.2;
+            descuentoPago = 0;
+            break;
+    }
+
+    costoNeto = costoBruto + (costoBruto * descuentoDestino) + (costoBruto * descuentoPago);
+
+    alert("El costo final de su viaje es: $" + costoNeto);
+
 
 }
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,66 @@
 function mostrar()
 {
+	var hora;
+    var mensaje;
 
+	hora = document.getElementById("laHora").value;
+
+	/*switch (hora) {
+        case "6":
+		case "7":
+		case "8":
+		case "9":
+		case "10":
+		case "11":
+			alert("Es de mañana.");
+			break;
+		case "12":
+		case "13":
+		case "14":
+		case "15":
+		case "16":
+		case "17":
+		case "18":
+		case "19":
+			alert("Es de tarde.");
+			break;
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "20":
+        case "21":
+        case "22":
+        case "23":
+        case "24":
+            alert("Es de noche.");
+            break;
+		default:
+			alert("La hora no existe.")
+			break;
+	}*/
+    switch(hora){
+        default:
+            if(hora>=6 && hora<=11)
+            {
+                mensaje="Es de mañana.";
+            }
+            else
+            {
+                if(hora<=19)
+                {
+                    mensaje="Es de tarde.";
+                }
+                else
+                {
+                    mensaje="Es de noche."
+                }
+                
+            }
+            break;
+    }
+
+    alert(mensaje);
 }
